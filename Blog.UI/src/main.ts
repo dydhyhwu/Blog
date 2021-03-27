d904484bc23c9d5885752e8056ca3aa331c524a0
若羽 <dydhyh@foxmail.com>
2021-03-27 16:38:56 +0800

    添加初始化步骤。

--- Blog.UI/src/main.ts
+++ Blog.UI/src/main.ts
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import Startup from "@/infrastructure/startup";
+import install from "vuetify-extension";
 
 Vue.config.productionTip = false;
+
+install(Vue, vuetify);
+Startup.init();
 
 new Vue({
   router,
