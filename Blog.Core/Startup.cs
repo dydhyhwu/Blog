9afbb2aa6c7e59a53470bb73163295d057257851
吴俊毅 <dydhyh@foxmail.com>
2022-01-10 09:42:42 +0800

    deps: 升级依赖。

--- Blog.Core/Startup.cs
+++ Blog.Core/Startup.cs
@@ -1,10 +1,12 @@
-﻿using Blog.Core.Infrastructure.Orm;
-using Blog.Core.Service;
-using Blog.Core.Service.Impl;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ZeroSum.App.Extensions;
+using ZeroSum.App.Middlewares;
 using ZeroSum.DependencyInjection.Attributes;
 using ZeroSum.DependencyInjection.Interfaces;
+using ZeroSum.Extend.EFCore.MySql;
+using ZeroSum.Extend.Swagger;
 
 namespace Blog.Core;
 
@@ -13,6 +15,13 @@ public class Startup : IAppStartup
 {
     public void ConfigurationServices(IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDistributedMemoryCache();
+        
+        services.AddHttpContextAccessor();
+        services.AddControllers().AddFriendlyResponse();
+        services.AddCors();
+        services.AddEFCore(configuration);
+        services.AddSwaggerDoc(configuration);
         // services.AddScoped<FavoriteLinkService, FavoriteLinkServiceImpl>();
         // services.AddScoped<Queries, QueriesImpl>();
         // services.AddScoped<ArticleService, ArticleServiceImpl>();
@@ -20,5 +29,26 @@ public class Startup : IAppStartup
         // services.AddScoped<ICosService, CosService>();
         // services.AddScoped<ICloudProviderService, CloudProviderService>();
         // services.AddScoped<ICloudAccountService, CloudAccountService>();
+    }
+
+    public void Configuration(IApplicationBuilder app)
+    {
+        // 全局异常处理
+        app.EnableExceptionHandler();
+
+        // 自动提交数据库改动
+        // app.EnableAutoSave();
+
+        app.UseCors();
+        app.UseSwaggerDoc();
+
+        app.UseRouting();
+
+        // app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
     }
 }
