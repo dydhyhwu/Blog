ed9be8089719f4f045348462b20e27cb3e37dc28
吴俊毅 <dydhyh@foxmail.com>
2022-01-14 14:11:51 +0800

    feat: 分离路由配置文件

---
+++ Blog.UI/src/router/web.ts
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
+import RouteGenerator from 'ea-router';
+import DefaultLayout from '@/components/DefaultLayout.vue';
+import PageNotFound from '@/components/PageNotFound.vue';
+
+Vue.use(VueRouter);
+
+const generator = new RouteGenerator(
+    require.context('../views/web', true, /\.vue$/)
+);
+
+generator.setDefaultLayout(DefaultLayout);
+generator.setNotFoundPage(PageNotFound);
+
+const routes: Array<RouteConfig> = generator.generate();
+
+const router = new VueRouter({
+    routes,
+});
+
+export default router;
