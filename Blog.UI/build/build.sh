8539be62b79fd3f0cb95c401f58ad2495e34f79c
吴俊毅 <dydhyh@foxmail.com>
2022-01-17 11:48:11 +0800

    ci: 修改复制路由文件的路径为相对路径

--- Blog.UI/build/build.sh
+++ Blog.UI/build/build.sh
@@ -1,11 +1,11 @@
 cd ../
 
 if [[ $BUILD_MODE = 'ALL' ]]; then
-    cp -f src/router/all.ts src/router/index.ts
+    cp -f ./src/router/all.ts ./src/router/index.ts
 
 elif [[ $BUILD_MODE = 'WEB' ]]; then
-    cp -f src/router/web.ts src/router/index.ts
+    cp -f ./src/router/web.ts ./src/router/index.ts
 
 elif [[ $BUILD_MODE = 'MANAGE' ]]; then
-    cp -f src/router/admin.ts src/router/index.ts
+    cp -f ./src/router/admin.ts ./src/router/index.ts
 fi
