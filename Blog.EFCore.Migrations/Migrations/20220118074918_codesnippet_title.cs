03b62fd23e081884c2185f9d6e0c1f188a21ee94
吴俊毅 <dydhyh@foxmail.com>
2022-01-18 15:50:05 +0800

    feat: 添加数据库迁移脚本

---
+++ Blog.EFCore.Migrations/Migrations/20220118074918_codesnippet_title.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.EFCore.Migrations.Migrations
+{
+    public partial class codesnippet_title : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "CodeSnippet",
+                type: "varchar(128)",
+                maxLength: 128,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "CodeSnippet");
+        }
+    }
+}
