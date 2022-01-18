50496398a55d7d8a7921d23feaccd7e41851fa44
吴俊毅 <dydhyh@foxmail.com>
2022-01-18 11:13:35 +0800

    feat: 添加数据库迁移。

--- Blog.EFCore.Migrations/Migrations/20220118031315_codesnippet.Designer.cs
+++ Blog.EFCore.Migrations/Migrations/20220118031315_codesnippet.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZeroSum.Extend.EFCore.MySql.EntityFrameworks;
 
@@ -10,9 +11,10 @@ using ZeroSum.Extend.EFCore.MySql.EntityFrameworks;
 namespace Blog.EFCore.Migrations.Migrations
 {
     [DbContext(typeof(ZeroSumDbContext))]
-    partial class ZeroSumDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220118031315_codesnippet")]
+    partial class codesnippet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Blog.EFCore.Migrations/Migrations/20220118031315_codesnippet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.EFCore.Migrations.Migrations
+{
+    public partial class codesnippet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CodeSnippet",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Language = table.Column<string>(type: "varchar(32)", maxLength: 32, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Content = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreateTime = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CodeSnippet", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CodeSnippet");
+        }
+    }
+}
